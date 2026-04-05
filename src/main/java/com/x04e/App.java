02343cbf1e261fa12177208b5410d5916e88826f
Liam Hilliard <liam.h.-@hotmail.com>
2026-04-05 14:40:50 +0100

    Add LoadWorker error logging

--- src/main/java/com/x04e/App.java
+++ src/main/java/com/x04e/App.java
@@ -1,5 +1,7 @@
 package com.x04e;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebView;
@@ -17,6 +19,14 @@ public class App extends Application {
 
 		web.getEngine().load("https://google.com");
 		//web.getEngine().loadContent("<!DOCTYPE html><head><style>*{box-sizing: border-box;} html,body{box-sizing:border-box;height:100%;width:100%;background-color:red;}</style></head><html><body>Hello World!</body></html>", "text/html");
+
+		web.getEngine().getLoadWorker().exceptionProperty().addListener(new ChangeListener<Throwable>(){
+			@Override
+			public void changed(ObservableValue<? extends Throwable> observable, Throwable oldValue, Throwable newValue) {
+				System.out.print("LoadWorkerError: ");
+				newValue.printStackTrace();
+			}
+		});
 
         stage.setScene(new Scene(new StackPane(web), 800, 600));
         stage.show();
